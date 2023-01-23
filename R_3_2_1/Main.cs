d85e51af3b899a77019371c846ef72e0e5b57b85
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2023-01-23 22:24:10 +0500

    Добавьте файлы проекта.

---
+++ R_3_2_1/Main.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI_3_2_1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Duct> fInstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_DuctCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Duct>()
+                .ToList();
+
+            TaskDialog.Show("Количество воздуховодов", fInstances.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
